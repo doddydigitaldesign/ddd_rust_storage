2d28e5e6f48511e0374f044a7272341ea6178858
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-04-24 17:55:07 +0200

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,59 @@
 #[cfg(test)]
 mod tests {
+    use super::{Middleware, ReducerFunction, Store, Subscriber};
     #[test]
     fn it_works() {
+        #[derive(Eq, PartialEq, PartialOrd, Debug)]
+        enum ActionType {
+            Hello = 0,
+            Goodbye = 1,
+        }
+
+        #[derive(Default)]
+        struct State {
+            message: &'static str,
+        }
+
+        fn reducer(_state: &State, action_type: &ActionType) -> State {
+            match action_type {
+                ActionType::Hello => State {
+                    message: "Hello there!",
+                },
+                ActionType::Goodbye => State {
+                    message: "Goodbye for now...",
+                },
+            }
+        }
+
+        let mut my_store = Store::new(reducer, State::default());
+        fn middleware(_: &mut Store<State, ActionType>, action: ActionType) -> Option<ActionType> {
+            match action {
+                ActionType::Hello => Some(ActionType::Goodbye),
+                ActionType::Goodbye => Some(ActionType::Hello),
+            }
+        }
+        my_store.apply_middleware(middleware);
+        my_store.subscribe(|state: &State| {
+            println!("State changed: {}", state.message);
+        });
+
+        my_store.dispatch(ActionType::Hello);
+        my_store.dispatch(ActionType::Goodbye);
+
+        fn replacing_reducer(_state: &State, action_type: &ActionType) -> State {
+            match action_type {
+                ActionType::Hello => State {
+                    message: "Hello, the reducer has been replaced!",
+                },
+                ActionType::Goodbye => State {
+                    message: "Goodbye for real.",
+                },
+            }
+        }
+
+        my_store.replace_reducer(replacing_reducer);
+        my_store.dispatch(ActionType::Hello);
+        my_store.dispatch(ActionType::Goodbye);
         assert_eq!(2 + 2, 4);
     }
 }
